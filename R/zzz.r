7b7c211ad7f68f13068e9f256629b4990ad74476
Joe Swintek <swintek.joe@epa.gov>
2020-05-01 05:50:24 +0000

    version 0.9.5

--- R/zzz.r
+++ R/zzz.r
@@ -4,10 +4,10 @@ Temp  <- new.env()
 
 
 .onLoad<-function(...){
-#' @import gWidgets
+#' @import gWidgets2
 #' @import RGtk2
 #' @import R2HTML
-#' @import gWidgetsRGtk2	
+#' @import gWidgets2RGtk2	
 #' @importFrom grDevices dev.off heat.colors tiff pdf bmp jpeg png gray.colors rainbow terrain.colors topo.colors cm.colors
 #' @importFrom graphics barplot par
 #' @importFrom methods is 
